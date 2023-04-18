bf6701e325f5cc8916f6fef0c54d1f20feb9dbf6
Walter Castro <waltercstr28@gmail.com>
2023-04-18 10:50:04 -0700

    added javascript on shop which adds items to cart

---
+++ js/shop.js
@@ -0,0 +1,33 @@
+const addToCartBtn = document.getElementById('addToCartBtn');
+  addToCartBtn.addEventListener('click', addToCart);
+
+  let cartItems = [];
+
+  const addToCartButtons = document.querySelectorAll(".card .btn-dark");
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", addToCart);
+  });
+
+  function addToCart(event) {
+    const card = event.target.closest(".card");
+    const title = card.querySelector(".card-title").textContent;
+    const price = 6.99;
+    const item = { title, price };
+    cartItems.push(item);
+    updateCart();
+  }
+  
+  function updateCart() {
+    const tableBody = document.querySelector("#shoppingCartModal tbody");
+    let html = "";
+    cartItems.forEach((item) => {
+      html += `
+        <tr>
+          <td>${item.title}</td>
+          <td>$${item.price.toFixed(2)}</td>
+          <td>1</td>
+        </tr>
+      `;
+    });
+    tableBody.innerHTML = html;
+  }
